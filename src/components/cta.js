8068f94e4139e6f660e67e9b605e805e40f046bc
Jared Haagen <jared.haagen@gmail.com>
2025-01-29 06:40:31 -0500

    closed 12u registration

--- src/components/cta.js
+++ src/components/cta.js
@@ -5,7 +5,7 @@ function CTA() {
 return (
 <div className="cta">
 <h2>Spring 2025 Registration is open!</h2>
-            <p>Registration for all team is currently open. Spots fill up quickly.</p>
+            <p>Registration for the 14U team is currently open. <strong>The 12U registration is now CLOSED.</strong></p>
             <p><a href="https://forms.gle/VXgr5iQB8ymxXFfB9">Register Today!</a></p>
             </div>
 
