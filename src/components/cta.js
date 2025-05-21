80d675f9425d5d2aa612bb95c743609a69e74eef
Jared Haagen <jared.haagen@gmail.com>
2025-05-20 21:11:07 -0400

    updated CTA for summer lacrosse

--- src/components/cta.js
+++ src/components/cta.js
@@ -4,8 +4,9 @@ import './cta.scss';
 function CTA() {
 return (
 <div className="cta">
-<h2>Spring 2025 Registration is now CLOSED!</h2>
-            <p>Registration for the 12U and 14U team is CLOSED for his season.</p>
+<h2>Summer Lacrosse - PSM Lacrosse</h2>
+            <p>PSM is offering a middle school instructional league. June 10th - July 24th. More information available at the website linked below.<br/><br/>
+                <a href="https://bondsports.co/activity/programs/CO_ED-adult-OTHER/6755/season/PSM%20Middle%20School%20Instructional%20League/65546">Visit PSM's website</a></p>
             </div>
 
 );
