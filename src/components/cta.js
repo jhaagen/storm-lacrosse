48a48bccec9ee8940fe90a6ea2ed4ba8f2d26b63
Jared Haagen <jared.haagen@gmail.com>
2026-01-08 06:26:08 -0500

    Updated to state 14u team is full, but we are still looking to fill the 10U and 12U teams.

--- src/components/cta.js
+++ src/components/cta.js
@@ -5,7 +5,7 @@ function CTA() {
 return (
 <div className="cta">
  <h2>Spring Registration is Open</h2>
-            <p>Registration is open for the 2026 spring season. Please fill out the registration form and the medical form to secure your spot on the team. </p>
+            <p>Registration is open for the 2026 spring season. The 14U team is full, but we are still looking to fill the 10U and 12U teams. Please fill out the registration form and the medical form to secure your spot on the team. </p>
             <p><a href="/registration">Register Now</a></p>
             </div>
 
